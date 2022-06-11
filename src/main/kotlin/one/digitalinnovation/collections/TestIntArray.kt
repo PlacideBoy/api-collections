12be0b0d03271e01f676687b5ae65e82c0fe16f3
JorgeLucas <83237763+PlacideBoy@users.noreply.github.com>
2022-06-11 11:27:00 -0300

    finalizaçãu IntArray

--- src/main/kotlin/one/digitalinnovation/collections/TestIntArray.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TestIntArray.kt
@@ -12,4 +12,24 @@ fun main() {
     for (valor in values) {
         println(valor)
     }
+
+    println("======================")
+
+    values.forEach {
+        println(it)
+    }
+
+    println("======================")
+
+    for (index in values.indices) {
+        println(values[index])
+    }
+
+    println("======================")
+
+    values.sort()
+
+    for (valor in values) {
+        println(valor)
+    }
 }
