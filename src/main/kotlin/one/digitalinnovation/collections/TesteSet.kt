61c47937d33a092cbec3ac7f48c6ce277ac5ebc4
JorgeLucas <83237763+PlacideBoy@users.noreply.github.com>
2022-06-11 14:31:24 -0300

    teste conjuntos

--- src/main/kotlin/one/digitalinnovation/collections/TesteSet.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteSet.kt
@@ -7,4 +7,19 @@ fun main() {
 
     val funcionarios1 = setOf(joao, pedro)
     val funcionarios2 = setOf(maria)
+
+    val resultUnion = funcionarios1.union(funcionarios2)
+    resultUnion.forEach { println(it) }
+
+    println("------------------------------------------")
+
+    val funcionarios3 = setOf(joao, pedro, maria)
+    val resultSubtract = funcionarios3.subtract(funcionarios2)
+    resultSubtract.forEach { println(it) }
+
+    println("------------------------------------------")
+
+
+    val resultIntersect = funcionarios3.intersect(funcionarios2)
+    resultIntersect.forEach { println(it) }
 }
